533473f532cc0fffb5273f665b600fa3c06a06c4
adamkr98 <adamkrynski2@gmail.com>
2023-06-20 10:27:15 +0200

    I added a padding-left in filters

--- script.js
+++ script.js
@@ -74,6 +74,34 @@ categories.addEventListener("click", (event) => {
     }
 })
 
+const http = require('http');
+const fs = require('./meals.json');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+const filePath = 'path/to/file.json';
+
+const server = http.createServer((req, res) => {
+  fs.readFile(filePath, 'utf8', (error, data) => {
+    if (error) {
+      res.statusCode = 500;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('Error reading JSON file');
+      console.error(error);
+      return;
+    }
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(data);
+  });
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+
 
 //////////footer ///////////
 //contact Form
